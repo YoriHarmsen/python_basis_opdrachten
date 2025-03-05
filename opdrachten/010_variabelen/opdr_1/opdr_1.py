716e641ccaeff2beebd0b629912da808735c1e45
YoriHarmsen <97086799@st.deltion.n>
2025-03-05 13:46:25 +0100

    Mijn eerste 2 opdrachten

--- opdrachten/010_variabelen/opdr_1/opdr_1.py
+++ opdrachten/010_variabelen/opdr_1/opdr_1.py
@@ -1,7 +1,9 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student: Yori Harmsen
+# Groep:IT2A
 
 # Hier komt je code...
 
-my_list = ...
+my_list = [13, "wat een weertje", 12.5, 8]
+print (my_list)
+         
--- opdrachten/010_variabelen/opdr_2/opdr_2.py
+++ opdrachten/010_variabelen/opdr_2/opdr_2.py
@@ -1,7 +1,9 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student:Yori Harmsen 
+# Groep:IT1A
 
 # Hier komt je code...
 
-my_tuple = ...
+my_tuple = [11, "Oh een tuple", 13.5, 8009]
+print(my_tuple)
+            
